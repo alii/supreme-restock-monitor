542218438f3688a463cb82d34a3fee9a1cbd1c24
Alistair Smith <aabbccsmith@users.noreply.github.com>
2018-11-16 07:22:08 +0000

    update

--- userscript.js
+++ userscript.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Supreme Restock Monitor
 // @namespace    https://majorcraft.xyz
-// @version      1
-// @description  Monitor Restocks on Supreme Newyork
+// @version      1.1
+// @description  try to take over the world!
 // @author       aabbccsmith
 // @match        https://www.supremenewyork.com/shop/*/*/*
 // @grant        none
@@ -10,10 +10,17 @@
 // ==/UserScript==
 (function() {
     'use strict';
-    var discordWebhookUrl = "YOUR DISCORD WEBHOOK URL"; //Important! Make sure you set this when you install this script
+    var discordWebhookUrl = "YOUR WEBHOOK HERE";
     var isSoldOut = function(el) {
         return (document.querySelector(".sold-out")) ? true : false;
     }
+
+    if (window.sessionStorage.reload != "loaded") {
+
+        window.sessionStorage.reload = "loaded";
+        alert("This script will constantly run, and not allow any bots or other extensions to function!");
+    }
+    //This script sends a discord notification whenever an item on supremenewyork.com restocks. The tab must be open for this script to work
     if(!isSoldOut(document.getElementById("add-remove-buttons"))) {
         $.ajax({
             data: JSON.stringify({"content":window.location + " has just restocked!"}),
@@ -28,4 +35,6 @@
     } else {
         window.location.reload();
     }
+
+
 })();
